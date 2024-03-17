23ba001ce1e0dc928f067867c9964c7fda10ce48
Marcos Saito <developer.saito@gmail.com>
2024-03-17 19:50:33 -0300

    chore: imrpove index layout

--- src/pages/LandingPage/LandingPageComponent.tsx
+++ src/pages/LandingPage/LandingPageComponent.tsx
@@ -1,6 +1,13 @@
 import styles from "./LandingPage.module.css";
 import { Link } from "react-router-dom";
 
+import BookSvg from "@icons/book.svg?react";
+import BrainSvg from "@icons/brain.svg?react";
+import ChalkboardSvg from "@icons/chalkboard.svg?react";
+import NotesSvg from "@icons/notes.svg?react";
+import BackpackSvg from "@icons/backpack.svg?react";
+import GraduationSvg from "@icons/graduation.svg?react";
+
 function LandingPageComponent() {
     const currentYear = new Date().getFullYear();
     return (
@@ -16,72 +23,83 @@ function LandingPageComponent() {
             </header>
             <main className={styles.LandingPageContent}>
                 <section className={styles.Headline}>
-                    <ul>
-                        <li>Aunos</li>
-                        <li>Ensino</li>
-                        <li>Estudos</li>
-                        <li>Vestibular</li>
-                        <li>Comunidade</li>
-                    </ul>
-                    <h1>Transforme seu Potencial em Excelência</h1>
-                    <h2>
-                        Acompanhamento Personalizado para Alunos do Ensino Médio
-                        <br />
-                        Rumo ao Sucesso no Vestibular!
-                    </h2>
-                    <button>Registrar</button>
+                    <div className={styles.Left}>
+                        <BookSvg />
+                        <GraduationSvg style={{ marginLeft: "-5rem" }} />
+                        <BrainSvg />
+                    </div>
+                    <div className={styles.Middle}>
+                        <ul>
+                            <li>Alunos</li>
+                            <li>Ensino</li>
+                            <li>Estudos</li>
+                            <li>Vestibular</li>
+                            <li>Comunidade</li>
+                        </ul>
+                        <h1>Transforme seu Potencial em Excelência</h1>
+                        <h2>
+                            Acompanhamento Personalizado para Alunos do Ensino
+                            Médio
+                            <br />
+                            Rumo ao Sucesso no Vestibular!
+                        </h2>
+                        <div className={styles.Buttons}>
+                            <button>Logar</button>
+                            <button>Registrar</button>
+                        </div>
+                    </div>
+                    <div className={styles.Right}>
+                        <ChalkboardSvg />
+                        <div style={{ marginRight: "-5rem" }}>
+                            <BackpackSvg />
+                        </div>
+                        <NotesSvg />
+                    </div>
                 </section>
                 <section className={styles.Features}>
-                    <img src="/img/trophy.svg" />
-                    <h2>Bem-vindo à Vogarify</h2>
-                    <h3>
-                        Aqui a sua jornada do ensino médio se transforma em uma
-                        trajetória de sucesso no vestibular!
-                    </h3>
-                    <ul className={styles.FeatureList}>
-                        <li className={styles.Feature}>
-                            <article>
-                                <i className="fa-solid fa-bullseye" />
-                                <h4>Conquiste Seu Melhor Desempenho</h4>
-                                <p>
-                                    Nosso objetivo é fazer com que os alunos
-                                    alcancem resultados extraordinários. Seja
-                                    você um aluno com desempenho ruim buscando a
-                                    excelência ou alguém já com bom desempenho
-                                    aspirando à perfeição, nossa plataforma é
-                                    seu parceiro de jornada.
-                                </p>
-                            </article>
-                        </li>
-                        <li className={styles.Feature}>
-                            <article>
-                                <i className="fa-solid fa-book" />
-                                <h4>Cobertura Completa do Ensino Médio</h4>
-                                <p>
-                                    [Seu Nome da Plataforma] abrange todas as
-                                    disciplinas ensinadas no ensino médio,
-                                    seguindo a grade curricular do MEC. Nossa
-                                    abordagem holística garante que você esteja
-                                    preparado para qualquer desafio no
-                                    vestibular.
-                                </p>
-                            </article>
-                        </li>
-                        <li className={styles.Feature}>
-                            <article>
-                                <i className="fa-solid fa-dumbbell" />
-                                <h4>Simulados Direcionados</h4>
-                                <p>
-                                    Ao realizar simulados, você receberá uma
-                                    análise personalizada. Descubra quais
-                                    matérias são seus pontos fortes e onde você
-                                    precisa focar mais esforços. [Nome da
-                                    Plataforma] coloca você no controle de sua
-                                    preparação.
-                                </p>
-                            </article>
-                        </li>
-                    </ul>
+                    <div className={styles.Title}>
+                        <img src="/img/trophy.svg" />
+                        <h2>Bem-vindo à Vogarify</h2>
+                    </div>
+                    <article className={styles.Feature}>
+                        <h3>Conquiste Seu Melhor Desempenho</h3>
+                        <p>
+                            Nosso objetivo é fazer com que os alunos alcancem
+                            resultados extraordinários. Seja você um aluno com
+                            desempenho ruim buscando a excelência ou alguém já
+                            com bom desempenho aspirando à perfeição, nossa
+                            plataforma é seu parceiro de jornada.
+                        </p>
+                    </article>
+                    <article className={styles.Feature}>
+                        <h3>Cobertura Completa do Ensino Médio</h3>
+                        <p>
+                            Vamos abranger todas as disciplinas ensinadas no
+                            ensino médio, seguindo a grade curricular do MEC.
+                            Nossa abordagem holística garante que você esteja
+                            preparado para qualquer desafio no vestibular.
+                        </p>
+                    </article>
+                    <article className={styles.Feature}>
+                        <h3>Simulados Direcionados</h3>
+                        <p>
+                            Ao realizar simulados, você receberá uma análise
+                            personalizada. Descubra quais matérias são seus
+                            pontos fortes e onde você precisa focar mais
+                            esforços. Colocamos você no controle de sua
+                            preparação.
+                        </p>
+                    </article>
+                    <article className={styles.Feature}>
+                        <h3>Acompanhamento Individualizado</h3>
+                        <p>
+                            Nossa plataforma oferece feedback personalizado para
+                            ajudá-lo a melhorar constantemente. Estamos aqui
+                            para guiar sua jornada, garantindo que cada
+                            exercício e cada minuto dedicado aos estudos leve
+                            você mais perto do sucesso.
+                        </p>
+                    </article>
                 </section>
             </main>
             <footer>
